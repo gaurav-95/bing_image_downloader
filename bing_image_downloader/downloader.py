4af141124c298750ba0f85cf7b968b2dde9ece83
Gaurav Hazra <gaurav.hazra1995@gmail.com>
2024-03-12 19:14:07 +0530

    Update downloader.py
    Return just image links

--- bing_image_downloader/downloader.py
+++ bing_image_downloader/downloader.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 import shutil
 from pathlib import Path
 
@@ -8,34 +9,41 @@ except ImportError:  # Python 3
     from .bing import Bing
 
 
-def download(query, limit=100, output_dir='dataset', adult_filter_off=True, 
-force_replace=False, timeout=60, filter="", verbose=True):
+def download(query, limit=100, output_dir='dataset', adult_filter_off=True,
+             force_replace=False, timeout=60, filter="", verbose=True):
 
-    # engine = 'bing'
     if adult_filter_off:
         adult = 'off'
     else:
         adult = 'on'
 
-    
     image_dir = Path(output_dir).joinpath(query).absolute()
 
     if force_replace:
         if Path.is_dir(image_dir):
             shutil.rmtree(image_dir)
 
-    # check directory and create if necessary
     try:
         if not Path.is_dir(image_dir):
             Path.mkdir(image_dir, parents=True)
 
     except Exception as e:
-        print('[Error]Failed to create directory.', e)
+        print('[Error] Failed to create directory.', e)
         sys.exit(1)
-        
-    print("[%] Downloading Images to {}".format(str(image_dir.absolute())))
-    bing = Bing(query, limit, image_dir, adult, timeout, filter, verbose)
-    bing.run()
+
+    print("[%] Retrieving Image Links for {}".format(query))
+    bing = Bing(query, limit, adult, timeout, filter, verbose)
+    image_links = bing.run()
+
+    if not image_links:
+        print("[!] No image links retrieved. Exiting.")
+        sys.exit(1)
+
+    print("[%] Image Links:")
+    for index, link in enumerate(image_links, start=1):
+        print("[{}] {}".format(index, link))
+    
+    return image_links
 
 
 if __name__ == '__main__':
